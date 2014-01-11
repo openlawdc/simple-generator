45d80d22e3484df9bc2adeb430054a253eb9ffb4
Tom MacWright <tom@macwright.org>
2014-01-11 12:21:11 -0500

    Start citations, continue to tweak recursive'ish level handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var finder = require('findit')(process.argv[2] || '.'),
     path = require('path'),
+    Citation = require('citation'),
     _ = require('lodash'),
     fs = require('fs');
 
@@ -46,8 +47,15 @@ function onfile(file, stat) {
             if (f.level.level && !Array.isArray(f.level.level)) {
                 f.level.level = [f.level.level];
             }
+            if (f.level.text && !Array.isArray(f.level.text)) {
+                f.level.text = [f.level.text];
+            }
+            if (!f.level.text) f.level.text = [];
             fs.writeFileSync(file.replace('json', 'html'),
-                templates.section(f));
+                templates.section({
+                    d: f,
+                    cited: cited
+                }));
         } else if (f.level.type == 'Division') {
             fs.writeFileSync(file.replace('json', 'html'),
                 templates.division(f));
@@ -75,5 +83,64 @@ function onfile(file, stat) {
         } else {
             console.log(f.level.type);
         }
+    }
+}
+
+function cited(text) {
+    var c = Citation.find(text, {
+        context: {
+            dc_code: {
+                source: 'dc_code'
+            }
+        },
+        excerpt: 40,
+        types: ['dc_code', 'dc_register', 'law', 'stat'],
+        replace: {
+            dc_code: codeCited,
+            law: lawCited,
+            dc_register: dcrCited,
+            stat: statCited
+        }
+    }).text;
+
+    return c;
+
+    function linked(url, text) {
+        return "<a href='" + url + "'>" + text + "</a>";
+    }
+
+    function statCited(cite) {
+        if (parseInt(cite.stat.volume, 10) < 65)
+            return;
+
+        return linked('http://api.fdsys.gov/link?collection=statute&volume=' + cite.stat.volume + '&page=' + cite.stat.page,
+            cite.match);
+    }
+
+    // is this a current DC Code cite (something we should cross-link),
+    // or is it to a prior version of the DC Code?
+    function codeCited(cite) {
+        var index = cite.excerpt.search(/ior\s+codifications\s+1981\s+Ed\.?\,?/i);
+        if (index > 0 && index < 40) // found, and to the left of the cite
+            return;
+
+        return linked("./" + cite.dc_code.title + "-" + cite.dc_code.section + '.html',
+            cite.match);
+    }
+
+    function lawCited(cite) {
+        var lawName = cite.law.type + " law " + cite.law.congress + "-" + cite.law.number;
+        var url = 'http://www.govtrack.us/search?q=' + encodeURIComponent(lawName);
+        return linked(url, cite.match);
+    }
+
+    // just link to that year's copy on the DC Register website
+    function dcrCited(cite) {
+        if (parseInt(cite.dc_register.volume, 10) < 57)
+            return;
+
+        var year = parseInt(cite.dc_register.volume, 10) + 1953;
+        return linked('http://www.dcregs.dc.gov/Gateway/IssueList.aspx?IssueYear=' + year,
+            cite.match);
     }
 }
