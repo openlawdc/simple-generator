7872e8ea58403655923adb50690e5e36f82de6f7
Joshua Tauberer <jt@occams.info>
2014-01-25 09:37:08 -0500

    add a lot of code documentation and tweak a bit of the logic

--- index.js
+++ index.js
@@ -11,36 +11,41 @@ var body_template = _.template(fs.readFileSync('templates/section._'));
 
 var recency_info = parse_xml_file(basedir + "/index.xml").find("meta/recency").text;
 
-finder.on('directory', ondirectory)
-    .on('file', onfile);
-
+// Load pre-made indexes that help us locate the filename for any given section (by citation),
+// and to locate the parent and children of any page.
 var section_to_filename = JSON.parse(fs.readFileSync(basedir + '/section_index.json'));
 var section_to_parent = JSON.parse(fs.readFileSync(basedir + '/section_parents_index.json'));
 var section_to_children = JSON.parse(fs.readFileSync(basedir + '/section_children_index.json'));
 
+// Recursively process the code files.
 function ondirectory(dir, stat, stop) {
     var base = path.basename(dir);
     if (base === '.git' || base === 'node_modules') stop();
 }
-
 function onfile(file, stat) {
     // run a specific file by putting it on the command line
     if (process.argv.length > 3 && !file.match(process.argv[3])) return;
     if (file.match(/\.xml$/))
         convert_file(file);
 }
+finder.on('directory', ondirectory)
+    .on('file', onfile);
 
+// Utility function to load an XML file using elementtree.
 function parse_xml_file(file) {
     var xml = fs.readFileSync(file).toString(); // not sure why toSting is needed to make a string
     return et.parse(xml)._root;
 }
 
+// Main function to convert a code XML file to its HTML rendering.
 function convert_file(file) {
     console.log(file);
 
+    // Load the file & sanity check that this is actually a file for the DC Code.
     var dom = parse_xml_file(file);
     if (dom.tag != "level") return;
 
+    // Make links to the child levels from this page (i.e. moving down in the table of contents).
     // Map xs:include's to information we need to make a link.
     var children = dom.findall("ns0:include").map(function(node) {
         var fn = path.dirname(file) + "/" + node.get('href');
@@ -53,22 +58,24 @@ function convert_file(file) {
         }; });
 
     // Get the <text> and <level> nodes that make up the body
-    // and flatten it out so the template doesn't have to deal with
+    // and flatten them out so the template doesn't have to deal with
     // the recursive nature of <level> nodes.
     var body_paras = [];
     flatten_body(dom, body_paras);
 
-    // Set indentation_pixels and the has-level-num on paragraphs with a level-num span.
+    // Set the 'has-level-num' class on paragraphs with a level-num span
+    // so that the CSS can take care of the un-indentation needed to get
+    // level numbering in the right place.
     body_paras.forEach(function(para) {
-        para.indentation_pixels = 30*para.indentation;
         para.text.forEach(function(span) {
             if (span.class == "level-num")
                 para.class += " has-level-num";
         });
     });
 
-    // Also split the paragraphs into subgroups so we can style the annotations
-    // separately from the main code content.
+    // Group the paragraphs into <div>s according to the 'grouop' value
+    // that we have set on each paragraph so that we can style ranges of
+    // paragraphs including annotations, form, and table-styled paragraphs.
     var body_groups = [ { group: null, paras: [] } ];
     body_paras.forEach(function(para) {
         if (para.group != body_groups[body_groups.length-1].group)
@@ -76,8 +83,8 @@ function convert_file(file) {
         body_groups[body_groups.length-1].paras.push(para);
     });
 
-
-    // Find the ancestors of this file.
+    // Find the ancestors of this file to show the navigation links to
+    // go up the table of contents to higher levels.
     var ancestors = [];
     var page_id = get_file_id(dom, file);
     var parent_id = page_id;
@@ -102,6 +109,8 @@ function convert_file(file) {
 }
 
 function make_page_link(page_id) {
+    /* This is a utility function to take a page id and make an object
+       with information needed to render a link to that page: href and title. */
     if (!page_id) return null;
     var file = section_to_filename[page_id];
     return {
@@ -111,6 +120,8 @@ function make_page_link(page_id) {
 }
 
 function make_page_title(obj) {
+    /* Create the canonical display name for a page.*/
+
     var level_type = obj.find("type").text;
 
     var title = null;
@@ -119,28 +130,37 @@ function make_page_title(obj) {
         // this is the root, just use the heading
 
     } else if (level_type == "Section") {
+        // this is a section, so show "§ XX-YY".
         title = "§ " + obj.find("num").text;
 
     } else if (level_type == "placeholder") {
+        // this is a placeholder, which has a bit of a complex display semantics
+
         var level_section = obj.find("section");
         var level_section_start = obj.find("section-start");
         var level_section_end = obj.find("section-end");
         var level_section_range_type = obj.find("section-range-type");
 
+        // placeholder for a single section
         if (level_section)
-            title = "§" + level_section.text;
+            title = "§ " + level_section.text;
+
+        // placeholder for a range of sections "XXX-YYY"
         else if (level_section_range_type.text == "range")
             title = "§" + level_section_start.text + "-§" + level_section_end.text;
+
+        // placeholder for two (typically consecutive) sections, "XXX, YYY"
         else if (level_section_range_type.text == "list")
             title = "§" + level_section_start.text + ", §" + level_section_end.text;
 
     } else {
-        // Division, Title, Part, etc.
+        // "Division I", "Title 10", "Part XXX", etc.
         title = level_type;
         if (obj.find("num"))
             title += " " + obj.find("num").text;
     }
 
+    // Concatenate the level's heading text.
     var level_heading = obj.find("heading");
     if (level_heading) {
         if (!title)
@@ -150,47 +170,111 @@ function make_page_title(obj) {
         title += level_heading.text;
     }
 
-    // For placeholders.
+    // For placeholders, indicate the reason for the placeholder in brackets at the end.
     var level_reason = obj.find("reason");
     if (level_reason)
-        title += " (" + level_reason.text + ")";
+        title += " [" + level_reason.text + "]";
 
     return title;
 }
 
 function flatten_body(node, paras, indentation, parent_node_text, parent_node_indents, para_group) {
+    /* This function flattens the recursive nesting of <level> and <text> nodes that make up
+       the body content of a page. Each call fills node's paragraphs into the 'paras' argument.
+
+       As we descend into the hierarchy, 'indentation' is incremented by one. The top-level call
+       doesn't pass indentation or further arguments, so it comes in first as undefined, which is
+       why we use (indentation||0) below to turn it into a number.
+
+       The numbering on levels is (semantically) encoded with the <level> but in display appears
+       within its first child paragraph. For instance:
+
+          <level>
+            <num>(a)</num>
+            <heading>In General</heading>
+            <text>Lorem ipsum</text>.
+          </level>
+
+        Is rendered as:
+
+          (a) In General -- Lorem ipsum.
+
+        So at each level, we pass the level's number & heading down in parent_node_text and place
+        it into the flattened text when we encounter the first child.
+
+        There is a more complex case though. Consider:
+
+          <level>
+            <num>(a)</num>
+            <level>
+              <num>(1)</num>
+              <text>Lorem ipsum</text>.
+            </level>
+            <level>
+              <num>(2)</num>
+              <text>Dolor sit amet.</text>.
+            </level>
+          </level>
+
+        This is rendered as:
+
+          (a) (1) Lorem ipsum.
+              (2) Dolor sit amet.
+
+        Two odd things happen in this case. First, we stack two levels worth of numbering into
+        the same paragraph "(a) (1)". Second, the indentation of the first paragraph is pulled
+        back. Even though "Lorem ipsum" would have an 'indentation' of 1 (versus zero), the "(a)"
+        has to get rendered at an indentation of zero. When we hit stacked numbering like this,
+        we increment 'parent_node_indents' as we recurse into the children to track that we have
+        to have special un-indentation in the paragraph where that numbering gets serialized into.
+
+        Finally, we add a group value to each paragraph ('para_group'), which allows us to have
+        CSS rendering that spans consecutive paragraphs of the same group (e.g. indentation, form,
+        and table paragraphs).
+        */
+
     node.getchildren()
         .filter(function(node) { return node.tag == "text" || node.tag == "level" })
         .forEach(function(child, i) {
             if (child.tag == "text") {
+                // Create a paragraph object and add it to 'paras'.
+
+                // Check if we have any numbering or headings
                 var initial_text = [];
                 var my_indentation = indentation || 0;
                 if (i == 0 && parent_node_text) {
                     if (para_group) {
+                        // In any of the special paragraph classes that we track for grouping,
+                        // if there is any numbering or heading text coming from a parent level
+                        // don't display it inline in this paragraph. Make a separate heading
+                        // for it.
                         paras.push({ text: parent_node_text, indentation: indentation, class: "subheading", group: para_group });
                     } else {
-                        // This is the first paragraph within a level. Put the
-                        // parent number and heading here.
+                        // This is the first paragraph within a level and we have a number or
+                        // heading from its parent to display. Put the parent number and heading here.
                         initial_text = parent_node_text;
                         my_indentation -= parent_node_indents;
                     }
                 }
 
-                paras.push({ text: initial_text.concat(flatten_text(child)), indentation: my_indentation, group: para_group, class: "" });
+                // Flatten the text of the child, which takes care of turning mixed-content
+                // text like "lorem<span>ipsum</span> dolor" into a flat array of objects.
+                var child_text = flatten_text(child);
+
+                // Append the paragraph.
+                paras.push({ text: initial_text.concat(child_text), indentation: my_indentation, group: para_group, class: "" });
 
             } else if (child.tag == "level") {
+                // What group will we pass down into the child? If this level is of a certain type,
+                // then pass that group information down into all child paragraphs here.
                 var child_para_group = para_group;
-
                 var type = child.find("type");
                 if (type && ["annotations", "appendices", "form", "table"].indexOf(type.text) >= 0) {
                     child_para_group = type.text;
-                    //paras.push({ text: [{text: type.text}], indentation: indentation||0, group: child_para_group, class: "" });
                 }
 
-                // TODO: form and table
-
                 // Don't display the level's num and heading here but rather on the first
-                // paragraph within the level. 
+                // paragraph within the level.
                 var my_num_heading = [];
                 if (child.find("num")) my_num_heading.push( { text: child.find("num").text + " ", class: "level-num" } );
                 if (child.find("heading")) my_num_heading.push( { text: child.find("heading").text + (child_para_group ? "" : " — "), class: "level-heading" } );
@@ -218,6 +302,14 @@ function flatten_body(node, paras, indentation, parent_node_text, parent_node_in
 }
 
 function flatten_text(node) {
+    /* Turns mixed content <text> nodes into a flat array of text with styling information.
+       For example:
+          lorem<span style="xyz">ipsum</span>
+       turns into
+          [ {text: "lorem"}, {text: "ipsum", style: "xyz"} ]
+       Also HTML-escape the text and do citation link processing.
+    */
+
     var ret = [];
     ret.push({ text: cited(node.text), class: "", style: "" })
     node.getchildren()
@@ -229,6 +321,9 @@ function flatten_text(node) {
 }
 
 function cited(text) {
+    /* Add links to any recognized citations. */
+
+    // HTML-escape the content first because we'll be adding raw HTML for the links.
     function escape_html(html) {
       return String(html)
         .replace(/&/g, '&amp;')
@@ -240,6 +335,7 @@ function cited(text) {
 
     text = escape_html(text);
 
+    // Process citations.
     var c = Citation.find(text, {
         context: {
             dc_code: {
@@ -308,7 +404,8 @@ function cited(text) {
 }
 
 function get_file_id(dom, file) {
-    // this is duplicated in make_index.js, except for the +1 for a slash
+    // Gets the "id" of a file, which is how it appears in the pre-made index files.
+    // (this is duplicated in make_index.js, except for the +1 for a slash)
     var fn = file.substring(basedir.length+1).replace(".xml", "");
     if (dom.find("type").text == "Section")
         return dom.find("num").text;
@@ -317,6 +414,9 @@ function get_file_id(dom, file) {
 }
 
 function get_section_range(id, start_or_end, depth) {
+    /* Gets the first (start_or_end=0) or last (start_or_end=1) section number
+       of all descendants of this page in the code. */
+
     var children = section_to_children[id];
 
     // base case has to be a Section or placeholder level... except in weird
@@ -327,26 +427,40 @@ function get_section_range(id, start_or_end, depth) {
             return null;
         }
 
+        // Parse the XML of the child.
         var dom = parse_xml_file(basedir + "/" + section_to_filename[id] + ".xml");
+
+        // For sections, return just the section number. Omit the section symbol
+        // because that's handled in the template.
         if (dom.find("type").text == "Section") {
             var num = dom.find("num").text;
             return num;
         }
+
+        // For placeholders...
         if (dom.find("type").text == "placeholder") {
             var level_section = dom.find("section");
             var level_section_start = dom.find("section-start");
             var level_section_end = dom.find("section-end");
+
+            // This placeholder stands in for a single section, so return that number.
             if (level_section)
                 return level_section.text;
+
+            // This placeholder stands in for a range of sections. Return the first or
+            // last component of the range depending on whether the caller is looking for
+            // the first or last section number.
             else
                 return start_or_end == 0 ? level_section_start.text : level_section_end.text;
         }
 
-        // Weird case where a big level has no children.
+        // This is a weird case where a big level has no children. Use our title string
+        // generator to provide a value.
         var title = make_page_title(dom);
         return title;
     }
 
+    // Recurse into either the first or last child, as appropriate.
     return get_section_range(
         children[start_or_end == 0 ? 0 : children.length-1],
         start_or_end,
@@ -354,24 +468,44 @@ function get_section_range(id, start_or_end, depth) {
 }
 
 function get_sibling(id, direction) {
+    /* Gets the previous (direction == -1) or next (direction == 1) document
+       in the Code starting at the document whose id is id.
+
+       The preceding document is the document's preceding sibling, if it has
+       one. This perhaps should be changed one day to be the preceding sibling's
+       last-most decedendant. Not sure if that'll be obvious in the UI though.
+        
+       If the document doesn't have a preceding sibling, then the previous
+       document will be its parent. If the document doesn't have a following
+       sibling then the next document will be its parent's next document. */
+
     if (!(id in section_to_parent)) return null;
 
+    // Get the parent document. If we're already at the root, just return null.
     var parent = section_to_parent[id];
     if (!(parent in section_to_children)) return null;
 
+    // Look for the previous/next sibling.
     var seen_me = false;
     var sibling = null;
     section_to_children[parent].forEach(function(child_id) {
+        // Looking for the first sibling after the document.
         if (seen_me && direction == 1 && !sibling)
             sibling = child_id;
 
         if (child_id == id) seen_me = true;
 
+        // Looking for the last sibling before the document.
         if (!seen_me && direction == -1)
             sibling = child_id;
     });
 
-    if (!sibling) sibling = get_sibling(parent, direction);
+    if (!sibling) {
+        if (direction == -1)
+            return parent;
+        else
+            return get_sibling(parent, direction);
+    }
     
     return sibling;
 }
