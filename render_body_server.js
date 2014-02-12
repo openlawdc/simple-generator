10cefbf2323974cca222927b0007d66565c253e8
Joshua Tauberer <jt@occams.info>
2014-02-12 10:20:27 -0500

    some improvements to render_body_server.js for my code editor prototype

--- render_body_server.js
+++ render_body_server.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var et = require('elementtree');
 var render_body = require('./render_body.js');
 
-var basedir = "code";
+var basedir = process.argv[2] || 'code';
 var section_to_filename = JSON.parse(fs.readFileSync(basedir + '/section_index.json'));
 var section_to_children = JSON.parse(fs.readFileSync(basedir + '/section_children_index.json'));
 
@@ -16,8 +16,25 @@ http.createServer(function (req, res) {
   req.on('data', function(chunk) {
     incoming_xml += chunk;
   }).on('end', function() {
-	  var dom = et.parse(incoming_xml)._root;
-	  var body = render_body.render_body("file.html", dom, section_to_filename, section_to_children, basedir);
+    var dom;
+    try {
+	   dom = et.parse(incoming_xml)._root;
+    } catch (e) {
+      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify( { "error": "Invalid XML: " + e } ));
+      res.end();
+      return;
+    }
+
+    var body;
+    try {
+	    body = render_body.render_body("file.html", dom, section_to_filename, section_to_children, basedir);
+    } catch (e) {
+      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify( { "error": "Something is wrong in the code: " + e } ));
+      res.end();
+      return;
+    }
 
 	  res.writeHead(200, {'Content-Type': 'text/html'});
 	  res.write(body.rendered)
